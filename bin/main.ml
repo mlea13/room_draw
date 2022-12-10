36bc8135f629da5f54a35e991485ee01652c6fd3
Maddy <maddy.lea@gmail.com>
2022-12-10 12:04:27 -0500

    name

--- bin/main.ml
+++ bin/main.ml
@@ -2,9 +2,15 @@ open Room_draw
 
 let stays : Stay.t list =
   [
-    { name = "Maddy"; days = 3 };
+    { name = "Morgan"; days = 1 };
+    { name = "Charlie"; days = 1 };
+    { name = "Louisa"; days = 1 };
+    { name = "Maddy"; days = 1 };
     { name = "Kenny"; days = 1 };
-    { name = "Imma"; days = 2 };
+    { name = "Evan"; days = 1 };
+    { name = "Julia"; days = 1 };
+    { name = "Imma"; days = 1 };
+    { name = "Frankie"; days = 1 };
   ]
 
 let () = randomize stays
